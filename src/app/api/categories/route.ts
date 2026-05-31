7b4d32cb92abad59744ad2123853bc17e8bf5aee
codezenashish <ixtashish@gmail.com>
2026-05-31 21:11:55 +0530

    feat: Add bookmark management features with categories and tags
    - Updated Prisma schema to include Bookmark, Category, and Tag models.
    - Introduced new API route for categories.
    - Added dotenv for environment variable management.
    - Integrated @dnd-kit/react for drag-and-drop functionality.
    - Created a new database connection file for Prisma.
    - Added a proxy for Clerk middleware to handle authentication.
    - Updated layout metadata and included a new favicon.

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import {}
+import { prisma } from "@/src/lib/db";
+
+export async function GET(){
+    try {
+        const  {userID} = auth()
+    } catch (error) {
+        
+    }
+}
