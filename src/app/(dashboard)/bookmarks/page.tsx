b64f7aa28b14795ab7ae84b180c237d07c07bed2
codezenashish <ixtashish@gmail.com>
2026-06-02 23:18:51 +0530

    feat: implement CategoryGridSection component and useCombobox hook for bookmark management

--- src/app/(dashboard)/bookmarks/page.tsx
+++ src/app/(dashboard)/bookmarks/page.tsx
@@ -1,12 +1,14 @@
-import PageHeader from '@/src/features/bookmarks/components/PageHeader'
-import React from 'react'
+import PageHeader from "@/src/features/bookmarks/components/PageHeader";
+import CategoryGridSection from "@/src/features/bookmarks/components/social-category-section";
+import React from "react";
 
 const page = () => {
   return (
     <>
-    <PageHeader title='Bookmarks' />
+      <PageHeader title="Bookmarks" />
+      <CategoryGridSection />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
