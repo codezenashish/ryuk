fb941d952520df7ff77cd62778cf3cd08518d79d
codezenashish <ixtashish@gmail.com>
2026-07-13 11:35:52 +0530

    feat: add QuickCraft component to the landing page

--- src/app/(landing)/quick-craft/page.tsx
+++ src/app/(landing)/quick-craft/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/features/quick-craft/components/header";
+import QuickCraft from "@/features/quick-craft/components/quick-craft";
 
 export default function Home() {
   return (
     <div className="w-full">
       <Header />
+      <QuickCraft />
     </div>
   );
 }
