8afea16300c3761230968ad2d52b0988ea70175f
codezenashish <ixtashish@gmail.com>
2026-08-11 23:14:16 +0530

    feat: implement bookmark management system including CRUD UI, category organization, and URL metadata auto-fetching

---
+++ src/features/bookmarks/index.ts
@@ -0,0 +1,8 @@
+export * from "./components/bookmark-card";
+export * from "./components/bookmark-grid";
+export * from "./components/bookmark-row";
+export { default as BookmarkList } from "./components/bookmark-list";
+export * from "./components/category-filter";
+export * from "./dialogs/add-bookmark-modal";
+export * from "./hooks/use-bookmark-queries";
+export * from "./utils/category-icon-registry";
